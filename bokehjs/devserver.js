ffcd4bae6fc6629b240d48b5ea13578fd7d38117
Adam Atkinson <adatkins@microsoft.com>
2017-06-20 10:52:54 -0400

    Add simple HTML file to use built BokehJS

--- bokehjs/devserver.js
+++ bokehjs/devserver.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
-
-const appRoot = require('app-root-path');
+var appRoot = require('app-root-path');
 
 app.route("/")
     .get((req, res) => {
         res.status(200).send({msg: "HELLO"});
     });
-//app.use("/compiledjs", express.static(__dirname + '/compiledjs'));
+
+app.route("/test")
+    .get((req, res) => {
+        res.sendFile("base_test.html", { root: appRoot.toString() });
+    });
+app.use("/build", express.static(__dirname + '/build'));
 
 // start server and listens on a port
 app.set('port', process.env.PORT || 3000);
